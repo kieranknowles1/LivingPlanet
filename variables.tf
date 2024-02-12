1d9be045b07c1e234963844f36ee773cbe9a21d8
kieranknowles1 <kieranknowles11@hotmail.co.uk>
2024-02-12 13:08:34 +0000

    Fix script

--- variables.tf
+++ variables.tf
@@ -1,17 +1,12 @@
-  # Region to deploy resources, pick something close to you
-  # NOTE: Some regions do not support all resources.
-  # NOTE: Please use the Azure cost calculator to estimate costs for your region.
-  # https://azure.microsoft.com/en-in/pricing/calculator/?cdn=disable
-  variable "region" {
-    default = "uksouth"
-  }
+# Region to deploy resources, pick something close to you
+# NOTE: Some regions do not support all resources.
+# NOTE: Please use the Azure cost calculator to estimate costs for your region.
+# https://azure.microsoft.com/en-in/pricing/calculator/?cdn=disable
+variable "region" {
+  default = "uksouth"
+}
 
-  # Prefix for all resources
-  variable "prefix" {
-      default = "TestDeployment"
-  }
-
-  # SSH public key to access the VM. The private key MUST NOT be shared under any circumstances
-  variable "ssh_key" {
-      default = file("~/.ssh/id_rsa.pub")
-  }
+# Prefix for all resources
+variable "prefix" {
+  default = "TestDeployment"
+}
