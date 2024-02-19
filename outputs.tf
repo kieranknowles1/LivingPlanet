6db1f01ace8f32f7dc68aa29cf4ad64a3aa57df0
kieranknowles1 <kieranknowles11@hotmail.co.uk>
2024-02-19 14:47:14 +0000

    Add missing change for last commit

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,8 @@ output "public_ip" {
   description = "The public IP address of the web server"
   value = azurerm_linux_virtual_machine.vm.public_ip_address
 }
+
+output "src_blob_url" {
+  description = "The URL of the source code blob"
+  value = azurerm_storage_blob.html.id
+}
