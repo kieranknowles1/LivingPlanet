07891a856441debb78e1d7ddc5a4ab234ad4fcba
kieranknowles1 <kieranknowles11@hotmail.co.uk>
2024-03-19 11:33:57 +0000

    Add marker to map

--- src/html/js/index.js
+++ src/html/js/index.js
@@ -1,3 +1,7 @@
+/**
+ * Initialize the map as a child of the given element
+ * @param {HTMLElement} element
+ */
 function initMap (element) {
   return new google.maps.Map(element, {
     center: { lat: 55, lng: -1.6 },
@@ -12,6 +16,21 @@ function initMap (element) {
   })
 }
 
+/**
+ * Create a marker on the map
+ * @param {google.maps.Map} map
+ * @param {google.maps.LatLng} position
+ * @param {string} title String to display on hover
+ */
+function createMarker (map, position, title) {
+  return new google.maps.Marker({ position, map, title })
+}
+
 jQuery(document).ready(() => {
-  initMap(document.getElementById('map'))
+  const map = initMap(document.getElementById('map'))
+
+  const marker = createMarker(map, { lat: 55, lng: -1.6 }, "A point between a football field and I don't know what the other thing is for.")
+  marker.addListener('click', () => {
+    alert('This is indeed the middle of a field')
+  })
 })
