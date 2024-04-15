1b4944c2c739e64a5a7536dae7b6c289d5928929
kieranknowles1 <kieranknowles11@hotmail.co.uk>
2024-04-15 13:57:48 +0100

    Use route API

--- site/static/js/index.js
+++ site/static/js/index.js
@@ -23,7 +23,42 @@ function initMap (element) {
  * @param {string} title String to display on hover
  */
 function createMarker (map, position, title) {
+  // NOTE: This is deprecated, but Google's API is developer-hostile
   return new google.maps.Marker({ position, map, title })
+}
+
+/**
+ * Get directions from one location to another
+ * @param {string} from
+ * @param {string} to
+ * @param {google.maps.TravelMode} method
+ */
+async function getDirections (from, to, method) {
+  const service = new google.maps.DirectionsService()
+
+  return await service.route({
+    origin: from,
+    destination: to,
+    travelMode: method
+  })
+}
+
+// We reuse the same renderer so that only one route is displayed at a time
+let directionsRenderer
+/**
+ * Render the route on the map
+ * @param {google.maps.Map} map
+ * @param {google.maps.DirectionsResult} route
+ * @param {HTMLElement} panel
+ */
+function renderRoute (map, route, panel) {
+  if (!directionsRenderer) {
+    directionsRenderer = new google.maps.DirectionsRenderer()
+    directionsRenderer.setMap(map)
+  }
+
+  directionsRenderer.setDirections(route)
+  directionsRenderer.setPanel(panel)
 }
 
 jQuery(document).ready(() => {
@@ -32,5 +67,20 @@ jQuery(document).ready(() => {
   const marker = createMarker(map, { lat: 55, lng: -1.6 }, "A point between a football field and I don't know what the other thing is for.")
   marker.addListener('click', () => {
     alert('This is indeed the middle of a field')
+  })
+
+  $('#directions').on('click', async () => {
+    const from = $('#from').val()
+    const to = $('#to').val()
+    const method = $('#method').val()
+    const panel = $('#directions-panel').get(0)
+
+    if (!from || !to) {
+      alert('Please enter both a start and end location')
+      return
+    }
+
+    const route = await getDirections(from, to, method)
+    renderRoute(map, route, panel)
   })
 })
--- site/views/pages/index.blade.php
+++ site/views/pages/index.blade.php
@@ -7,13 +7,24 @@ title: Home
 @section('head')
 	<!-- Not bothered about putting the API key here as it's restricted to the domain, the key is free, and it's linked to a card with £0.22 on it -->
     <script
-		defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBk0ovRwzRYtQfLcraqO5rGM3-adTsGTnM&callback=console.debug&libraries=maps,marker&v=beta&loading=async"
+		defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBk0ovRwzRYtQfLcraqO5rGM3-adTsGTnM&callback=console.debug&libraries=maps,marker&v=3.56&loading=async"
 	></script>
 	<script src="/js/index.js" defer></script>
 @endsection
 
 @section('content')
-	<section class="map-container" id="map">
+	<section>
 		<h2>Map</h2>
+		<input id="from" type="text" placeholder="From" />
+		<input id="to" type="text" placeholder="To" />
+		<select id="method">
+			<option value="DRIVING">Driving</option>
+			<option value="WALKING">Walking</option>
+			<option value="BICYCLING">Cycling</option>
+			<option value="TRANSIT">Public Transport</option>
+		</select>
+		<button id="directions">Get Directions</button><br>
+		<div class="map-container" id="map"></div>
+		<div id="directions-panel"></div>
 	</section>
 @endsection
