06a3bfe792089de1058c5a5d6676c21e36c512f0
kieranknowles1 <kieranknowles11@hotmail.co.uk>
2024-04-16 11:06:05 +0100

    Split weather getters into a module

--- site/static/js/weather.js
+++ site/static/js/weather.js
@@ -1,20 +1,12 @@
-// I don't care about exposing this in Git, it isn't linked to any payment information
-const OPENWEATHER_KEY = 'a80e01c7ed75b3d74ba3a66bbd39c09f'
-
-const QUALITY_BG_COLORS = [
-  '#00ff00', // Good
-  '#ffff00', // Fair
-  '#ff7f00', // Moderate
-  '#ff0000', // Poor
-  '#7f00ff' // Very Poor
-]
-const QUALITY_FG_COLORS = [
-  '#000000', // Good
-  '#000000', // Fair
-  '#000000', // Moderate
-  '#ffffff', // Poor
-  '#ffffff' // Very Poor
-]
+import {
+  describeAirQuality,
+  getAirQualityIndex,
+  getCurrentWeather,
+  getLocationName,
+  getPollution,
+  QUALITY_BG_COLORS,
+  QUALITY_FG_COLORS
+} from './modules/weatherData.mjs'
 
 // Newcastle Upon Tyne
 const DEFAULT_LOCATION = {
@@ -22,7 +14,7 @@ const DEFAULT_LOCATION = {
   lon: -1.6
 }
 
-function getLocation () {
+function getQueryLocation () {
   const query = new URLSearchParams(window.location.search)
   const lat = query.get('lat')
   const lon = query.get('lon')
@@ -37,65 +29,25 @@ function getLocation () {
   }
 }
 
-async function getLocationName (location) {
-  const data = await $.getJSON(`https://api.openweathermap.org/geo/1.0/reverse?lat=${location.lat}&lon=${location.lon}&limit=1&appid=${OPENWEATHER_KEY}`)
-
-  if (data.length === 0) {
-    return 'Unknown Location'
-  }
-  return data[0].name
-}
-
-jQuery(document).ready(async () => {
+jQuery(document).ready(() => {
   createAirQualityKey()
 
-  const location = getLocation()
-  const locationName = await getLocationName(location)
-  console.log(locationName)
-  $('#location').text(locationName)
-  $('#lat').text(location.lat)
-  $('#lon').text(location.lon)
-
-  getCurrentWeather(OPENWEATHER_KEY, location.lat, location.lon)
-  getPollution(OPENWEATHER_KEY, location.lat, location.lon)
-})
-
-// Describe the air quality based on the AQI
-// Source: https://openweathermap.org/api/air-pollution
-function describeAirQuality (aqi) {
-  switch (aqi) {
-    case 1: return 'Good'
-    case 2: return 'Fair'
-    case 3: return 'Moderate'
-    case 4: return 'Poor'
-    case 5: return 'Very Poor'
-  }
-}
-
-function getCurrentWeather (key, lat, lon) {
-  $.getJSON(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=${key}`, data => {
-    const weather = data.weather[0]
-    $('#description').text(weather.description)
-    $('#temperature').text(data.main.temp)
-    $('#windSpeed').text(data.wind.speed)
+  const location = getQueryLocation()
+  getLocationName(location).then(locationName => {
+    $('#location').text(locationName)
+    $('#lat').text(location.lat)
+    $('#lon').text(location.lon)
   })
-}
 
-/**
- * Get the air quality index for the given value and thresholds.
- * @param {number} value The value to get the air quality index for.
- * @param {number[]} thresholds The thresholds for the air quality index.
- */
-function getAirQualityIndex (value, thresholds) {
-  // Get the highest threshold that the value is greater than or equal to
-  for (let i = thresholds.length - 1; i >= 0; i--) {
-    if (value >= thresholds[i]) {
-      return i + 1 // +1 as the threshold is for the NEXT index
-    }
-  }
-  // Below the lowest threshold, consider the air quality good
-  return 0
-}
+  getCurrentWeather(location.lat, location.lon).then(response => {
+    const weather = response.weather[0]
+    $('#description').text(weather.description)
+    $('#temperature').text(response.main.temp)
+    $('#windSpeed').text(response.wind.speed)
+  })
+
+  fillPollutionSection(location.lat, location.lon)
+})
 
 // Generate a key for the pollutant elements
 // Do this in JS to ensure it uses the same colors as the pollutant elements
@@ -121,21 +73,21 @@ function updatePollutantElement (id, value, qualityThresholds) {
     .css('color', QUALITY_FG_COLORS[quality])
 }
 
-function getPollution (key, lat, lon) {
-  $.getJSON(`https://api.openweathermap.org/data/2.5/air_pollution?lat=${lat}&lon=${lon}&appid=${key}`, data => {
-    $('#airQualityIndex').text(data.list[0].main.aqi)
-    $('#airQualityDescription').text(describeAirQuality(data.list[0].main.aqi))
+async function fillPollutionSection (lat, lon) {
+  const data = await getPollution(lat, lon)
 
-    const components = data.list[0].components
-    // Update the pollutant elements
-    // Threshold sources: https://openweathermap.org/api/air-pollution
-    updatePollutantElement('co', components.co, [4400, 9400, 12400, 15400])
-    $('#no').text(components.no) // Not in AQI
-    updatePollutantElement('no2', components.no2, [40, 70, 150, 200])
-    updatePollutantElement('o3', components.o3, [60, 100, 140, 180])
-    updatePollutantElement('so2', components.so2, [20, 80, 250, 350])
-    $('#nh3').text(components.nh3) // Not in AQI
-    updatePollutantElement('pm2_5', components.pm2_5, [10, 25, 50, 75])
-    updatePollutantElement('pm10', components.pm10, [20, 50, 100, 200])
-  })
+  $('#airQualityIndex').text(data.list[0].main.aqi)
+  $('#airQualityDescription').text(describeAirQuality(data.list[0].main.aqi))
+
+  const components = data.list[0].components
+  // Update the pollutant elements
+  // Threshold sources: https://openweathermap.org/api/air-pollution
+  updatePollutantElement('co', components.co, [4400, 9400, 12400, 15400])
+  $('#no').text(components.no) // Not in AQI
+  updatePollutantElement('no2', components.no2, [40, 70, 150, 200])
+  updatePollutantElement('o3', components.o3, [60, 100, 140, 180])
+  updatePollutantElement('so2', components.so2, [20, 80, 250, 350])
+  $('#nh3').text(components.nh3) // Not in AQI
+  updatePollutantElement('pm2_5', components.pm2_5, [10, 25, 50, 75])
+  updatePollutantElement('pm10', components.pm10, [20, 50, 100, 200])
 }
